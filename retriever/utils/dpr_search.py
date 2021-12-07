df2471708ac825ed982b59d8c64b756a8d3308eb
levuloihust99 <levuloihust@outlook.com>
2021-12-07 10:37:16 +0000

    add utils/dpr_search.py

---
+++ retriever/utils/dpr_search.py
@@ -0,0 +1,12 @@
+from typing import Text
+import requests
+import json
+
+URL = "http://103.252.1.223:5051/retrieve"
+
+
+def search(query, top_docs: int):
+    headers = {'Content-Type': 'application/json'}
+    data = {'query': query, 'top_docs': 10}
+    resp = requests.post(url=URL, headers=headers, data=json.dumps(data))
+    return resp.json()
